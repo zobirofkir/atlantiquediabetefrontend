2aa2f495b892ba43241b4b8cc6f3a769d2cc3537
zobirofkir <zobirofkir19@gmail.com>
2024-10-29 16:00:52 +0100

    update

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -4,7 +4,7 @@ const FooterComponent = () => {
   const name = localStorage.getItem('name');
   
   return (
-    <div className="text-center bg-gradient-to-r from-blue-500 to-blue-600 w-full">
+    <div className="text-center bg-gradient-to-r from-blue-500 to-blue-600 w-full md:px-0 px-4">
       <p className="text-xl text-center py-4 text-white font-bold tracking-wide">
         &copy; 2024 { name ?? 'Société Marocaine de Diabétologie' }. All rights reserved.
       </p>
