83aae65b68a5c4f5968e206714d39f39dbf83bea
zobirofkir <zobirofkir19@gmail.com>
2024-10-29 10:19:34 +0100

    traduire login screen fo frensh

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -23,13 +23,13 @@ const LoginScreen = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-blue-600 md:px-0 px-2">
       <div className="bg-white p-10 rounded-lg shadow-2xl w-full max-w-md">
-        <h2 className="text-3xl font-extrabold text-center text-gray-800 mb-6">SMD ADMIN LOGIN</h2>
+        <h2 className="text-3xl font-extrabold text-center text-gray-800 mb-6">SMD CONNEXION ADMINISTRATEUR</h2>
         
         {error && <p className="text-red-500 text-center mb-4 ">{error}</p>}
         
         <form onSubmit={handleLogin} className="space-y-6">
           <div>
-            <label className="block text-md font-medium text-gray-700 font-bold" htmlFor="email">Email Address</label>
+            <label className="block text-md font-medium text-gray-700 font-bold" htmlFor="email">Adresse email</label>
             <input
               type="email"
               id="email"
@@ -42,7 +42,7 @@ const LoginScreen = () => {
           </div>
 
           <div>
-            <label className="block text-md font-medium text-gray-700 font-bold" htmlFor="password">Password</label>
+            <label className="block text-md font-medium text-gray-700 font-bold" htmlFor="password">Mot de passe</label>
             <input
               type="password"
               id="password"
@@ -58,7 +58,7 @@ const LoginScreen = () => {
             type="submit"
             className="w-full py-3 bg-blue-600 text-white rounded-lg font-semibold hover:bg-blue-700 transition duration-200 shadow-lg"
           >
-            Login
+            Se connecter
           </button>
         </form>
       </div>
