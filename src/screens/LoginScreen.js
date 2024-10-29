05a1690ee50b7360fb0faae1be9f4683e8913434
zobirofkir <zobirofkir19@gmail.com>
2024-10-29 09:35:31 +0100

    style login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,60 +4,59 @@ import React, { useState } from 'react';
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(null); 
+  const [error, setError] = useState(null);
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const user = {
-      email,
-      password,
-    };
+    const user = { email, password };
 
     try {
       const response = await axios.post(`${process.env.REACT_APP_BACKEND_APP_URL}/api/auth/login`, user);
-      console.log(response.data.data);
       localStorage.setItem('accessToken', response.data.data.accessToken);
       window.location.href = '/dashboard';
     } catch (err) {
-      console.error(err);
       setError('Login failed. Please check your credentials.');
     }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm">
-        <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
-        {error && <p className="text-red-500 text-center mb-4">{error}</p>}
-        <form onSubmit={handleLogin}>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-700" htmlFor="email">Email</label>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-blue-600 md:px-0 px-2">
+      <div className="bg-white p-10 rounded-lg shadow-2xl w-full max-w-md">
+        <h2 className="text-3xl font-extrabold text-center text-gray-800 mb-6">SMD ADMIN LOGIN</h2>
+        
+        {error && <p className="text-red-500 text-center mb-4 ">{error}</p>}
+        
+        <form onSubmit={handleLogin} className="space-y-6">
+          <div>
+            <label className="block text-md font-medium text-gray-700 font-bold" htmlFor="email">Email Address</label>
             <input
               type="email"
               id="email"
-              className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:ring focus:ring-blue-300"
+              className="mt-1 block w-full border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring focus:ring-blue-400 focus:border-blue-500 transition duration-200"
               placeholder="you@example.com"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>
-          <div className="mb-6">
-            <label className="block text-sm font-medium text-gray-700" htmlFor="password">Password</label>
+
+          <div>
+            <label className="block text-md font-medium text-gray-700 font-bold" htmlFor="password">Password</label>
             <input
               type="password"
               id="password"
-              className="mt-1 block w-full border border-gray-300 rounded-md p-2 focus:ring focus:ring-blue-300"
+              className="mt-1 block w-full border border-gray-300 rounded-lg p-3 focus:outline-none focus:ring focus:ring-blue-400 focus:border-blue-500 transition duration-200"
               placeholder="********"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
+
           <button
             type="submit"
-            className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-500 transition duration-200"
+            className="w-full py-3 bg-blue-600 text-white rounded-lg font-semibold hover:bg-blue-700 transition duration-200 shadow-lg"
           >
             Login
           </button>
