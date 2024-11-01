8dd8df0b07816cce26858017e8f8313a8aa27e14
zobirofkir <zobirofkir19@gmail.com>
2024-11-01 10:02:30 +0100

    traduire footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -6,7 +6,7 @@ const FooterComponent = () => {
   return (
     <div className="text-center bg-gradient-to-r from-blue-500 to-blue-600 w-full md:px-0 px-4">
       <p className="text-xl text-center py-4 text-white font-bold tracking-wide">
-        &copy; 2024 { name ?? 'Société Marocaine de Diabétologie' }. All rights reserved.
+        &copy; 2024 { name ?? 'Société Marocaine de Diabétologie' }. Tous droits reserves.
       </p>
     </div>
   );
