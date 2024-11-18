00de2d52c7b633f4628658c91e7ed0640430913b
zobirofkir <zobirofkir19@gmail.com>
2024-11-18 10:22:54 +0100

    destruct header

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import jsPDF from 'jspdf';
 import * as XLSX from 'xlsx';
 import { inscriptionAction } from '../redux/actions/InscriptionAction';
 import { useDispatch, useSelector } from "react-redux";
+import HeaderComponent from '../components/HeaderComponent';
 
 const DashboardScreen = () => { 
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const { data, loading, error } = useSelector((state) => state.inscription);
 
@@ -22,14 +22,7 @@ const DashboardScreen = () => {
     }
   }, [navigate, dispatch]);
 
-  const handleLogout = () => {
-    localStorage.removeItem('accessToken');
-    navigate('/');
-  };
 
-  const toggleDropdown = () => {
-    setDropdownOpen((prev) => !prev);
-  };
 
   const exportToExcelForItem = (item) => {
     const worksheetData = [
@@ -138,39 +131,8 @@ const DashboardScreen = () => {
 
   return (
     <div className='container mx-auto py-12 px-6'>
-      {/* Header */}
-      <header className='flex justify-between items-center bg-gray-800 p-5 rounded-lg shadow-md text-white'>
-        <h1 className='text-xl font-bold'>Tableau de bord</h1>
-        <div className='relative'>
-          <button
-            onClick={toggleDropdown}
-            className='bg-red-500 px-3 py-2 rounded-full shadow-lg hover:bg-red-600 focus:outline-none'
-          >
-            <i className="fa-solid fa-right-from-bracket text-white"></i>
-          </button>
-          {dropdownOpen && (
-            <div className='absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10'>
-              <div className='py-3 px-4 text-gray-800 text-sm'>
-                <p>Etes-vous certains de vouloir vous déconnecter?</p>
-              </div>
-              <div className='flex border-t border-gray-200'>
-                <button
-                  onClick={handleLogout}
-                  className='w-full py-2 text-center text-red-600 hover:bg-red-100'
-                >
-                  Oui
-                </button>
-                <button
-                  onClick={toggleDropdown}
-                  className='w-full py-2 text-center text-gray-600 hover:bg-gray-100'
-                >
-                  Annuler
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
-      </header>
+
+    <HeaderComponent/>
 
       {/* Export Buttons */}
       <div className="flex md:justify-end justify-center mt-8 gap-4 ">
